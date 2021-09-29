430734aa3656a6455d5627ba2ce0e9a3cfe916f1
Antonio Yu <antonio.yu1998@gmail.com>
2021-09-28 20:47:22 -0500

    Added tables to the admin site. Added a location.json fixture

--- flytofasto_api/admin.py
+++ flytofasto_api/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Airline, Credit_card, Customer, Flight, Location, Ticket
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Airline)
+admin.site.register(Flight)
+admin.site.register(Location)
+admin.site.register(Ticket)
+admin.site.register(Credit_card)
